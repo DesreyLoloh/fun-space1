5d0858bd2f513fa38586c233e1dd17d61df5bce0
DesreyLoloh <desreykimberlyloloh@gmail.com>
2023-03-20 11:58:14 +0900

    proyek pertama saya

---
+++ app.py
@@ -0,0 +1,9 @@
+
+#Hitunglah segitiga sederhana
+def luas_segitiga() :
+    a = int(input("Masukan alas segitiga :"))
+    b = int(input("Masukan Tinggi segitiga :"))
+    luas = a * t / 2
+print("Luas segitiga adalah :", luas)
+
+luas_segitiga()
